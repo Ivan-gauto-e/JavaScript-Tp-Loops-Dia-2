3e1e9c9be95c81c8c34fce0875152642df89ce5f
Ivan Gauto <ivan.gauto@gm2dev.com>
2025-09-02 12:45:25 -0300

    Ejercicio 5 y 6 completos, ejercicio 7 incompleto

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //Ejercicio 1 - Duplicar Elementos con .map
-//ejercicio1();
 function ejercicio1() {
-  const Numeros = [1, 2, 3, 4, 5];
-  const Duplicados = Numeros.map((x) => x * 2);
+  const NUMEROS = [1, 2, 3, 4, 5];
+  const Duplicados = NUMEROS.map((x) => x * 2);
   console.log(Duplicados);
 }
+//ejercicio1();
 
 //Ejercicio 2 - Convertir Números a Strings con .map
 //ejercicio2();
 function ejercicio2() {
-  const Numeros2 = [1, 2, 3, 4, 5];
-  const NumeroAStrings = Numeros2.map((x) => x.toString());
+  const NUMEROS2 = [1, 2, 3, 4, 5];
+  const NumeroAStrings = NUMEROS2.map((x) => x.toString());
   console.log(NumeroAStrings);
 }
 
@@ -18,25 +18,48 @@ function ejercicio2() {
 //ejercicio3();
 function ejercicio3() {
   let suma = 0;
-  const Numeros3 = [1, 2, 3, 4, 5];
-  Numeros3.forEach((x) => {
+  const NUMEROS3 = [1, 2, 3, 4, 5];
+  NUMEROS3.forEach((x) => {
     suma += x;
   });
-  const promedio = suma / Numeros3.length;
+  const promedio = suma / NUMEROS3.length;
   console.log(promedio);
 }
 
 //Ejercicio 4 - Contar elementos con .forEach
-ejercicio4();
+//ejercicio4();
 function ejercicio4() {
   let contador = 0;
-  const Numeros4 = [4, 6, 10, 40, 90];
-  Numeros4.forEach((num) => {
+  const NUMEROS4 = [4, 6, 10, 40, 90];
+  NUMEROS4.forEach((num) => {
     num > 10 ? contador++ : null;
   });
   console.log(contador);
 }
 
 //Ejercicio 5 - Mostrar los cuadrados de los números del 1 al 10 usando .map
-ejercicio5();
-function ejercicio5() {}
+//ejercicio5();
+function ejercicio5() {
+  const Numeros5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const cuadrados = Numeros5.map((x) => x * x);
+  console.log(cuadrados);
+}
+
+//Ejercicio 6 - Sumar todos los números de un arreglo usando .forEach
+//ejercicio6();
+function ejercicio6() {
+  let suma = 0;
+  const NUMERO6 = [1, 2, 3, 4, 5];
+  NUMERO6.forEach((x) => (suma = suma + x));
+  console.log(suma);
+}
+
+//Ejercicio 7 - Mostrar los elementos de un arreglo con su posición (índice) usando .forEach
+function ejercicio7() {
+  const NOMBRES = ["Julian", "Roberto", "Martin", "Agustin"];
+  NOMBRES.forEach(x);
+  {
+    console.log("Indice: " + index + " Valor: " + numero);
+  }
+}
+ejercicio7();
